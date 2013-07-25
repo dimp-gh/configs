1445b4770df3f76b3e0f83d751c3d9f1970466ea
Dmitry Andreevich <dmand@yandex.ru>
2013-07-25 23:42:36 +0300

    Implement list of dependencies and add function for installing all them

--- emacs/.emacs.d/my-packages.el
+++ emacs/.emacs.d/my-packages.el
@@ -1,6 +1,26 @@
 (provide 'my-packages)
 
+;; There goes list of all packages i want to get installed
+(setq my-bundle
+      '(powerline
+	speedbar
+	auto-complete
+	yasnippet
+	elpy
+	scala-mode2
+	markdown-mode))
+
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+
+(defun install-my-bundle-packages ()
+  "Install only the sweetest of packages."
+  (interactive)
+  (package-refresh-contents)
+  (mapc '(lambda (package)
+           (unless (package-installed-p package)
+             (package-install package)))
+        my-bundle))
