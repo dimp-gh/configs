7cf5c1293848bfbf65de0187c37e01682d50c4d5
Dmitry Andreevich <dmand@yandex.ru>
2013-08-07 13:18:15 +0300

    Disable alternate theme for emacs in non-graphic mode

--- emacs/.emacs.d/my-color-theme.el
+++ emacs/.emacs.d/my-color-theme.el
@@ -3,9 +3,7 @@
 ;; solarized and monokai are accessible via 
 ;; color-theme-solarized and color-theme-monokai
 ;; nice grey theme
-(if (display-graphic-p)
-  (require 'zenburn-theme)
-  (load-theme 'tsdh-dark))
+(require 'zenburn-theme)
 ;; nice brown-red theme
 ;; (require 'birds-of-paradise-plus-theme)
 ;; almost nice tango theme
