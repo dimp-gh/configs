314b8a6fe1668bdc2193c6d50d398013498c3b69
Dmitry Andreevich <dmand@yandex.ru>
2013-08-10 12:18:13 +0300

    Refactor keybindings in emacs config

--- emacs/.emacs.d/my-key-bindings.el
+++ emacs/.emacs.d/my-key-bindings.el
@@ -1,8 +1,8 @@
 (provide 'my-key-bindings)
 ;; Additional key bindings for various functions
 
-;; redefined keys go here
-(global-set-key [S-M-down] 'win-resize-mi2nimize-vert)
+;; resising
+(global-set-key [S-M-down] 'win-resize-minimize-vert)
 (global-set-key [S-M-up] 'win-resize-enlarge-vert)
 (global-set-key [S-M-left] 'win-resize-minimize-horiz)
 (global-set-key [S-M-right] 'win-resize-enlarge-horiz)
@@ -11,13 +11,13 @@
 (global-set-key [S-M-left] 'win-resize-enlarge-vert)
 (global-set-key [S-M-right] 'win-resize-minimize-vert)
 
+;; moving between buffers with C-, and C-.
 (global-set-key [?\C-,] 'previous-buffer)
 (global-set-key [?\C-.] 'next-buffer)
 
+;; speedbar (not really often used)
 (global-set-key (kbd "<f11>") 'sr-speedbar-toggle)
 
-(global-set-key [C-kp-add] 'text-scale-increase)
-(global-set-key [C-kp-subtract] 'text-scale-decrease)
-
+;; universal hotkey for commenting/uncommenting code
 (global-set-key (kbd "C-x C-/") 'comment-region)
 (global-set-key (kbd "C-S-x C-?") 'uncomment-region)
