48b280a79a78723613dce730857069dd659e37b7
Dmitry Andreevich <dmand@yandex.ru>
2013-08-12 03:15:23 +0300

    Integrate calendar config eith overriding

--- emacs/.emacs.d/my-calendar-stuff.el
+++ emacs/.emacs.d/my-calendar-stuff.el
@@ -5,8 +5,10 @@
 ;; First day of the week
 (setq calendar-week-start-day 1) ; 0:Sunday, 1:Monday
 
+;; override that
+(setq my-calendar-private-url "https://www.google.com/calendar/XXX/basic.ics")
+
 (defun open-google-calendar ()
   "Open my Google calendar"
   (interactive)
-  (cfw:open-ical-calendar
-   "https://www.google.com/calendar/XXX/basic.ics")) ;; paste your ical link here
+  (cfw:open-ical-calendar my-calendar-private-url))
