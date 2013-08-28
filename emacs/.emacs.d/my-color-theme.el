d027410483751518c6c71504bc49add73563e8b8
Dmitry Andreevich <dmand@yandex.ru>
2013-08-28 18:30:06 +0300

    Moving from zenburn to my new eink theme

--- emacs/.emacs.d/my-color-theme.el
+++ emacs/.emacs.d/my-color-theme.el
@@ -3,9 +3,11 @@
 ;; solarized and monokai are accessible via 
 ;; color-theme-solarized and color-theme-monokai
 ;; nice grey theme
-(require 'zenburn-theme)
+;; (require 'zenburn-theme)
 ;; nice brown-red theme
 ;; (require 'birds-of-paradise-plus-theme)
 ;; almost nice tango theme
 ;; (require 'color-theme-tango)
 ;; color-theme-sanityinc-tomorrow-eighties is also quite nice
+(require 'eink-theme)
+(load-theme 'eink t)
