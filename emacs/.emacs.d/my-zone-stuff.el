9e0babf6208bba25624ffe7fdec92a3583df3b94
Dmitry Andreevich <dmand@yandex.ru>
2013-09-08 01:33:01 +0300

    Add conditional zoning

--- emacs/.emacs.d/my-zone-stuff.el
+++ emacs/.emacs.d/my-zone-stuff.el
@@ -2,4 +2,26 @@
 
 ;; zoning just for fun
 (require 'zone)
-(zone-when-idle 120)
+
+(defun get-active-process ()
+  (let ((command-result
+         (shell-command-to-string
+	  "/home/dmitry/code/active-window-process.sh"
+	  )))
+    (substring
+     command-result
+     0 (1- (length command-result)))))
+
+(defun is-emacs-active ()
+  "returns non-nil if emacs window is active right now"
+  (string-match "emacs"
+		(get-active-process)))
+
+(is-emacs-active)
+
+(defun zone-if-emacs-is-active ()
+  (when (is-emacs-active)
+    (zone)))
+
+;; zoning only if emacs window is active
+(setq zone-timer (run-with-idle-timer 120 t 'zone-if-emacs-is-active))
