d9a3eb8f384b4fbba3563a8f1303197d294692ec
Dmitry Andreevich <dmand@yandex.ru>
2013-11-01 15:25:40 +0300

    Add auto-modes for pascal sources

--- emacs/.emacs.d/my-auto-modes.el
+++ emacs/.emacs.d/my-auto-modes.el
@@ -4,3 +4,5 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
 (add-to-list 'auto-mode-alist '("\\.pl\\'" . prolog-mode))
+(add-to-list 'auto-mode-alist '("\\.pas\\'" . delphi-mode))
+(add-to-list 'auto-mode-alist '("\\.lpr\\'" . prolog-mode))
