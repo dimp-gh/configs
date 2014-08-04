611de563d61d5881adb7553a11c0c4012987b972
Dmitry Andreevich <dmand@yandex.ru>
2014-08-05 02:49:15 +0300

    Add org-mode sets to enable pdf export of cyrillic text

--- emacs/.emacs.d/my-org-stuff.el
+++ emacs/.emacs.d/my-org-stuff.el
@@ -4,3 +4,6 @@
 (setq org-todo-keywords
       (quote ((sequence "TODO(t!)" "STARTED(s!)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
               (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
+
+(add-to-list 'org-export-latex-packages-alist '("" "cmap" t))
+(add-to-list 'org-export-latex-packages-alist '("english,russian" "babel" t))
