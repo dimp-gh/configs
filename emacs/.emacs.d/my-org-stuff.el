3658b653f3370fe7b699db8375e2ac8db31efe25
Dmitry Andreevich <dmand@yandex.ru>
2014-08-07 04:05:15 +0300

    Update org-mode configs for fresh org-mode

--- emacs/.emacs.d/my-org-stuff.el
+++ emacs/.emacs.d/my-org-stuff.el
@@ -5,5 +5,7 @@
       (quote ((sequence "TODO(t!)" "STARTED(s!)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
               (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
 
-(add-to-list 'org-export-latex-packages-alist '("" "cmap" t))
-(add-to-list 'org-export-latex-packages-alist '("english,russian" "babel" t))
+;; assumes org-mode >= 8.0
+(require 'org-latex)
+(add-to-list 'org-latex-packages-alist '("" "cmap" t))
+(add-to-list 'org-latex-packages-alist '("english,russian" "babel" t))
